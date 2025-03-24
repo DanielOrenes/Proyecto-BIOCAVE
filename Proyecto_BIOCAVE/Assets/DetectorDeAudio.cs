ca27bdc1e5c25a44d59dd753fede97a1ca4e6aa2
OscarBlancoMon <oscar.blanco@campusfp.es>
2025-03-24 11:18:44 +0100

    Micro funcional (con error)

--- Proyecto_BIOCAVE/Assets/DetectorDeAudio.cs
+++ Proyecto_BIOCAVE/Assets/DetectorDeAudio.cs
@@ -14,7 +14,7 @@ public class DetectorDeAudio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void DetectarAudioMicro()
--- Proyecto_BIOCAVE/Assets/ParticulasDeMicrofono.cs
+++ Proyecto_BIOCAVE/Assets/ParticulasDeMicrofono.cs
@@ -6,7 +6,9 @@ public class ParticulasDeMicrofono : MonoBehaviour
     public ParticleSystem particulas;
     public DetectorDeAudio detector;
     public float sensitividad = 100;
-    public float rango = 0.1f;
+    public float rango = 2f;
+
+    public GameObject cubito;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,15 +20,19 @@ public class ParticulasDeMicrofono : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float volumen = detector.VolumenAudio(source.timeSamples, source.clip) * sensitividad;
+        float volumen = detector.obtenerVolumenMicro() * sensitividad;
+        Debug.Log(volumen);
 
         if(volumen <= rango && particulas.isPlaying == true)
         {
             particulas.Stop();
+
+            cubito.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
         }
         else if(volumen > rango && particulas.isStopped == true)
         {
             particulas.Play();
+            cubito.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
         }
     }
 
